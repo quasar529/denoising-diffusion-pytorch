31ed91f34eb55147764bc7cd9fda8129d0009930
quasar529 <quasar0529@gmail.com>
2024-07-02 20:17:29 +0900

    arrange out file

--- diffusion.sh
+++ diffusion.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #SBATCH --qos=base_qos
-#SBATCH --output=a6000_pal_val_2.out
+#SBATCH --output=../a6000_pal_val_2.out
 #SBATCH --time 0-23:00:00
 #SBATCH --gres=gpu:4
 eval "$(conda shell.bash hook)"
