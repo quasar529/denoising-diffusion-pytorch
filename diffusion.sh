e2719cd1e72ea755b4d57f04978c7e90a8872d0e
quasar429 <quasar0529@gmail.com>
2024-07-02 19:06:00 +0900

    exp: 4096

--- diffusion.sh
+++ diffusion.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 #SBATCH --qos=base_qos
-#SBATCH --output=a6000_pal_val_2.out
+#SBATCH --output=a6000_pretrain.out
 #SBATCH --time 0-23:00:00
 #SBATCH --gres=gpu:4
 eval "$(conda shell.bash hook)"
 conda activate tutorial
-accelerate launch main.py
+accelerate launch main.py --train_batch_size 4096 --save_and_sample_every 20000 
